8b27212ba361f057123a0293dca910e3361c9351
Nathan T Case <unclenate62@gmail.com>
2018-06-27 12:49:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,38 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(shout(string))
+}
+
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
+
+  if (`string`=== "I love you, Grandma!")
+  return `I love you, too.`
+  else if (`string` === 'lowercase')
+  return `I can't hear you!`
+  else if (`string` === `upperCase`)
+  return `YES INDEED!`
+}
